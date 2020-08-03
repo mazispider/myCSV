143a957b313a9ab26d980a532015a0ed0611525c
ml <mazispider@gmail.com>
2020-08-04 00:49:34 +0430

    added lNum interval

--- csv_parser.cpp
+++ csv_parser.cpp
@@ -10,10 +10,14 @@ int main(int argc, char **argv)
   fpath = std::string(argv[1]);
   colName = std::string(argv[2]);
   lNum = std::stoi(argv[3]);
+  
   std::cout << std::string(argv[1]) << std::endl;
   rapidcsv::Document doc(fpath);
-
+ 
   std::vector<float> col = doc.GetColumn<float>(colName);
+  if(lNum > col.size()){
+	  lNum = col.size();
+  }
   std::cout << "Read " << col.size() << " values in " << colName << " column."<< std::endl;
   for(int i = col.size() - lNum; i < col.size(); i++){
 	  std::cout << i << ": " << col[i] << std::endl;
